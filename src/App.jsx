6b15c8cd1e501ab10b487dd12ad09614bab3e33b
Balabharathi Jayaraman <balabharathi.jayaraman@gmail.com>
2021-12-26 00:50:15 +0100

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import React, {useState, useEffect} from 'react'
 function App (props) {
     const [timesClicked, setTimesClicked] = useState(0);
 
-    useEffect(() => {
-        document.getElementById('txt-times-clicked').innerHTML = `${timesClicked}`
-    })
     return (
-    <div>
-        <h1 className="text-2xl font-bold">Hello {props.name}! </h1>
-        <h4 id='txt-times-clicked'>timesClicked</h4>
-        <button onClick={() => setTimesClicked(timesClicked+1)}>Click Me</button>
+    <div className="p-6">
+        <h1 className="text-3xl font-medium">Practice React Application</h1>
+        <h4 className="text-sm mt-5 mb-5">{timesClicked} times.</h4>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"onClick={() => setTimesClicked(timesClicked+1)}>Click Me</button>
     </div>
     )
 }
