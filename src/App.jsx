8afecd8860c4c8a00734a976f63510b9969f3a8d
Balabharathi Jayaraman <balabharathi.jayaraman@gmail.com>
2021-12-26 00:36:17 +0100

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-function Hello (props) {
-    return <h1>Hello World! </h1>
+function App (props) {
+    const [timesClicked, setTimesClicked] = useState(0);
+
+    useEffect(() => {
+        document.getElementById('txt-times-clicked').innerHTML = `${timesClicked}`
+    })
+    return (
+    <div>
+        <h1 className="text-2xl font-bold">Hello {props.name}! </h1>
+        <h4 id='txt-times-clicked'>timesClicked</h4>
+        <button onClick={() => setTimesClicked(timesClicked+1)}>Click Me</button>
+    </div>
+    )
 }
 
-export default Hello;
+export default App;
